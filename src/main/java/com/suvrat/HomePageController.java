e1bef45b5a108eaf52d8cccd3e5e3a2e90924ef7
suvrat <suvrat12@gmail.com>
2018-05-27 23:06:39 +0530

    Logout funct

--- src/main/java/com/suvrat/HomePageController.java
+++ src/main/java/com/suvrat/HomePageController.java
@@ -1,13 +1,24 @@
 package com.suvrat;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomePageController {
 
 	@RequestMapping("/")
-	public String LoadHomePage() {
+	public String LoadHomePage(HttpServletRequest request,ModelMap m) {
+		HttpSession s = request.getSession();
+	    User u=  (User) s.getAttribute("up");
+	    
+	    if(u != null) {
+	    	
+	    	return "redirect:/home";
+	    }
 		return "index";
 	}
 	
--- src/main/webapp/WEB-INF/views/header.jsp
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 
@@ -21,7 +22,7 @@
 			<a class="navbar-brand" href="#">Ganesh Tutorials</a>
 		</div>
 		<ul class="nav navbar-nav navbar-right">
-			<li class="active"><a href="/">Login</a></li>
+			<li class="active"><a href="<c:url value="/" />" >Login</a></li>
 			<li class="active"><a href="registrationform">Register</a></li>
 		</ul>
 	</div>
