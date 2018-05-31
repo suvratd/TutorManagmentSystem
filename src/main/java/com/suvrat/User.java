a2ad44feaf595d15dbddb22e6acee5154b18cd0c
suvrat <suvrat12@gmail.com>
2018-06-01 01:07:42 +0530

    Q

--- src/main/java/com/suvrat/User.java
+++ src/main/java/com/suvrat/User.java
@@ -1,6 +1,7 @@
 package com.suvrat;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
 
 import com.suvrat.validator.IsUniqueForUserAndEmail;
+
 
 @Entity
 @Table(name="user")
